d472218fbb74be6490feb2b72ce1a2b9b14dc00e
Nella Smbatyan <112649804+NellaSmbatyan@users.noreply.github.com>
2022-12-04 19:45:32 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Request from "./Request/UsersPage";
+
+
+
+function App() {
+   
+    return (
+      <div className='App'>
+<Request/>
+     
+      </div>
+    )
+    }
+    
+    export default App;
+    
